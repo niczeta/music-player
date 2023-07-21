f9d4121337086370bc3c560a5b4db5ec899f365e
Nicole Misuraca <127198262+niczeta@users.noreply.github.com>
2023-07-21 21:07:25 +0200

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,46 @@
+let allMusic = [
+
+    {
+        name: "Entombed",
+        artist: "Deftones",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Me & You Together Song",
+        artist: "The 1975",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Still Beating",
+        artist: "Mac DeMarco",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Wicked Game",
+        artist: "Chris Isaak",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Bags",
+        artist: "Clairo",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "What Once Was",
+        artist: "Her's",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Telephones",
+        artist: "Vacation",
+        img: "music-7",
+        src: "music-7"
+    },
+    
+    ];
